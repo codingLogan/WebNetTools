0867b5427ae0415f417b3995af8e0701e19bddc8
logan gamebox <logan.razclan1@gmail.com>
2016-02-16 22:00:05 -0700

    shell

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
 echo date('Y-m-d H:i:s');
 echo "<hr>";
+echo "<pre>";
 
+$output = shell_exec("ls -al");
+echo $output;
+
+echo "<hr>";
 $output = shell_exec("sudo /usr/bin/arp-scan --localnet > localnet.txt");
+echo $output;
+
+echo "</pre>";
 
 echo "<hr>";
 phpinfo();
