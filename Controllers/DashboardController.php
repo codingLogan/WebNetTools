16ced67d09f059938dff2e7f7b0f7db0e271b112
logan gamebox <logan.razclan1@gmail.com>
2016-02-20 15:48:46 -0700

    title

--- Controllers/DashboardController.php
+++ Controllers/DashboardController.php
@@ -8,14 +8,13 @@ Date: 2/20/2016
 require_once "Models/Dashboard.php";
 
 class DashboardController {
-	private $dashboard;
+	public $dashboard;
 
 	public function __construct(){
 		$this->dashboard = new Dashboard();
 	}
 
 	public function start(){
-		echo "starting dashboard";
 		include "Views/dashboard/begin.html";
 		include "Views/dashboard/end.html";
 	}
--- dashboard.php
+++ dashboard.php
@@ -4,11 +4,9 @@ dashboard.php
 Author: Logan Rasmussen
 Date: 2/20/2016
 */
-echo "dashboard.php";
+
 require_once "Controllers/DashboardController.php";
-echo "past require";
 
 $controller = new DashboardController();
-var_dump($controller);
 $controller->start();
 ?>
