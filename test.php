b6bd4da9cee3e2f4bdc3196d994218dfb541df43
logan gamebox <logan.razclan1@gmail.com>
2016-03-10 19:49:40 -0700

    unique keys...

--- test.php
+++ test.php
@@ -8,13 +8,13 @@ require_once "functions/output_helpers.php";
 
 $array = array(
 	 "key" => "value"
-	,"key" => "value"
-	,"key" => "value"
-	,"key" => "value"
-	,"key" => "value"
-	,"key" => "value"
-	,"key" => "value"
-	,"key" => "value"
+	,"key1" => "value"
+	,"key2" => "value"
+	,"key3" => "value"
+	,"key4" => "value"
+	,"key5" => "value"
+	,"key6" => "value"
+	,"key7" => "value"
 );
 
 echo outputTable($array, array("keys","values"), true);
