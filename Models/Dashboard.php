914a905a7cdbd6b7e4e03aa7a7b1a1ad018a59b1
Logan Rasmussen <logan.rasmussen@enexsolutions.com>
2016-03-28 22:02:04 -0600

    no message

--- Models/Dashboard.php
+++ Models/Dashboard.php
@@ -22,8 +22,8 @@ class Dashboard {
 			,new NavLink("IP Geolocation", "geolocation.php")
 			,new NavLink("Ping", "ping.php")
 			,new NavLink("Scan Local IPs", "arpscan.php")
-			//,new NavLink("Wifi Scan", "wifi.php")
-			,new NavLink("Wireshark", "wireshark.php")
+			,new NavLink("Wifi Scan", "wifi.php")
+			//,new NavLink("Wireshark", "wireshark.php")
 			,new NavLink("Help", "help.php")
 		);
 		$this->tools_nav = array(
